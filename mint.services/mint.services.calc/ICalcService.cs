510cfbc8ec6e9367d38b155c46cc9b1edef80b1d
Miguel Santos <mig@migsan.com>
2016-02-07 02:20:14 +0000

    calc shortest path start

---
+++ mint.services/mint.services.calc/ICalcService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace mint.services.calc
+{
+  using models;
+
+  [ServiceContract]
+  public interface ICalcService
+  {
+    [OperationContract]
+    IEnumerable<IEnumerable<string>> GetShortestPath(string from, string to);
+  }
+}
